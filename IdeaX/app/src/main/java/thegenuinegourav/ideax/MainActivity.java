9fce12519152c6e306ec2e01ec4fb13166751ff0
gourav <gourav96suri@gmail.com>
2016-08-06 04:19:15 +0530

    Initial Commit

---
+++ IdeaX/app/src/main/java/thegenuinegourav/ideax/MainActivity.java
@@ -0,0 +1,27 @@
+package thegenuinegourav.ideax;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View view) {
+
+        startActivity(new Intent(this, HomeActivity.class));
+
+    }
+
+    public void SignUp(View view) {
+
+        startActivity(new Intent(this,HomeActivity.class));
+
+    }
+}
